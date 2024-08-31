b184eca6ca33dfac091341612cc4ecac8a040095
SebastianCotrina16 <sebastiancotrina16@gmail.com>
2024-08-31 10:38:55 -0500

    Storage Terraform Template [@SebastianCotrina16] #6

---
+++ terraform/modules/storage/variables.tf
@@ -0,0 +1,15 @@
+variable "storage_account_name" {
+  description = "The name of the Storage Account"
+  type        = string
+}
+
+variable "container_name" {
+  description = "The name of the Blob Storage Container"
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure region where resources will be deployed"
+  type        = string
+  default     = "East US"
+}
